1411c7594c995f1864f5c6e0fd1757b75e74ed2e
xxlvt <sopelkaboss@gmail.com>
2022-11-11 11:05:06 +0300

    client -> router update

--- client/src/Router/index.jsx
+++ client/src/Router/index.jsx
@@ -2,10 +2,17 @@ import {Routes, Route} from "react-router-dom";
 import {Layout} from "./Layout";
 import {Home} from "../components/Home";
 import {Product} from "../components/Product";
+import {Admin} from "../components/Admin";
+import {Profile} from "../components/Profile";
+import {Basket} from "../components/Basket";
 
 export const RouteNames = {
     HOME: '/',
-    PRODUCT: 'product'
+    ADMIN: '/admin',
+    PROFILE: '/profile',
+    BASKET: '/basket',
+    PRODUCT: '/product'
+
 }
 
 export const Router = () => {
@@ -13,6 +20,9 @@ export const Router = () => {
         <Routes>
             <Route path={RouteNames.HOME} element={<Layout/>}>
                 <Route index element={<Home/>}/>
+                <Route path={RouteNames.ADMIN} element={<Admin/>}/>
+                <Route path={RouteNames.PROFILE} element={<Profile/>}/>
+                <Route path={RouteNames.BASKET} element={<Basket/>}/>
                 <Route path={RouteNames.PRODUCT} element={<Product/>}/>
             </Route>
         </Routes>
