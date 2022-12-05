7725ab4fad0f48eb36e6c65787e6bd1021839fd4
xxlvt <sopelkaboss@gmail.com>
2022-12-05 19:33:08 +0300

    server -> Add error-middleware and api error

---
+++ server/src/errors/api.error.js
@@ -0,0 +1,43 @@
+class ApiError extends Error {
+
+	constructor(status, message, errors = []) {
+		super(message);
+		this.status = status;
+		this.errors = errors;
+	}
+
+	static ValidationError(error) {
+		return new ApiError(400, 'Ошибка валидации', error);
+	}
+
+	static PaginationError(message) {
+		return new ApiError(405, message);
+	}
+
+	static BadRequest(message) {
+		return new ApiError(400, message);
+	}
+
+	static NotFound(message) {
+		return new ApiError(404, message);
+	}
+
+	static UnauthorizedError() {
+		return new ApiError(401, 'Пользователь не авторизован!');
+	}
+
+	static NotAllowed(message) {
+		return new ApiError(405, message);
+	}
+
+	static NoContent(message) {
+		return new ApiError(204, message);
+	}
+
+	static MailingError(message) {
+		return new ApiError(550, message);
+	}
+}
+
+module.exports = {ApiError}
+
