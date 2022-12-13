c0045ee885cd8e4b0bb66ae709f96aa0f55283d6
xxlvt <sopelkaboss@gmail.com>
2022-12-14 01:18:34 +0300

    client -> add new icons

--- client/src/components/Admin/Icons/index.jsx
+++ client/src/components/Admin/Icons/index.jsx
@@ -5,6 +5,7 @@ import {CheckMark} from "./CheckMark";
 import {Cross} from "./Cross";
 import {Feedback} from "./Feedback";
 import {Home} from "./Home";
+import {Invisible} from "./Invisible";
 import {News} from "./News";
 import {Next} from "./Next";
 import {Order} from "./Order";
@@ -12,6 +13,7 @@ import {Product} from "./Product";
 import {ProductCategory} from "./ProductCategory";
 import {ProductFeature} from "./ProductFeature";
 import {User} from "./User";
+import {Visible} from "./Visible";
 
 export {
     Arrow,
@@ -21,11 +23,13 @@ export {
     Cross,
     Feedback,
     Home,
+    Invisible,
     News,
     Next,
     Order,
     Product,
     ProductCategory,
     ProductFeature,
-    User
+    User,
+    Visible
 }
