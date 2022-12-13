fd2af7bee903b15bb36eb8e636bf20063c977d7f
xxlvt <sopelkaboss@gmail.com>
2022-12-13 09:24:59 +0300

    client -> add new utils

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import bcrypt from 'bcryptjs';
 import {useEffect} from "react";
 import {useLocation} from "react-router-dom";
 
@@ -23,4 +24,22 @@ export function base64StringToFile(base64String, filename) {
         u8arr[n] = bstr.charCodeAt(n);
     }
     return new File([u8arr], filename, {type: mime});
+}
+
+export async function passwordHook(password) {
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt);
+}
+
+export function passwordValidate(password) {
+    return (password?.length > 5)
+}
+
+export function emailValidate(email) {
+    const EMAIL_REGEXP = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
+    return EMAIL_REGEXP.test(email);
+}
+
+export function userNameValidate(userName) {
+    return true
 }
