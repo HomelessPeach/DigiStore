053f8561e7524605689c33a730d2a5931a2d0de1
xxlvt <sopelkaboss@gmail.com>
2022-12-14 22:22:43 +0300

    client -> FeedbackTable

--- client/src/components/Admin/Tables/Feedbacks/FeedbackList.jsx
+++ client/src/components/Admin/Tables/Feedbacks/FeedbackList.jsx
@@ -3,6 +3,7 @@ import {feedbackAPI} from "../../../../services/FeedbackService";
 import {DataGrid} from "../../components/Datagrid";
 import {TextField} from "../../components/TextField";
 import {ListContainer} from "../TablesStyledBlocks";
+import {BoolField} from "../../components/BoolField";
 
 export const FeedbackList = () => {
 
@@ -11,7 +12,7 @@ export const FeedbackList = () => {
             <DataGrid getData={feedbackAPI.useFeedbackListQuery} idName={'feedback_id'}>
                 <TextField source={'feedback_id'} name={'id'} sortable={true}/>
                 <TextField source={'feedback_email'} name={'e-mail'} sortable={true}/>
-                <TextField source={'is_answer'} name={'Отвечено'} sortable={true}/>
+                <BoolField source={'is_answer'} name={'Отвечено'} sortable={true}/>
             </DataGrid>
         </ListContainer>
     )
--- client/src/components/Admin/Tables/Feedbacks/FeedbackShow.jsx
+++ client/src/components/Admin/Tables/Feedbacks/FeedbackShow.jsx
@@ -1,16 +1,27 @@
 import * as React from "react";
 import styled from "styled-components"
-import {userAPI} from "../../../../services/UserService";
+import {feedbackAPI} from "../../../../services/FeedbackService";
 import {useLocation} from "react-router-dom";
 import {AdminRouteNames} from "../../../../Router";
 import {TextField} from "../../components/TextField";
-import {ToolbarBlock, LinkButton, ShowContainer} from "../TablesStyledBlocks";
+import {ToolbarBlock, LinkButton, ShowContainer, EditToolbarBlock, Button} from "../TablesStyledBlocks";
+import {BoolField} from "../../components/BoolField";
+import {passwordHook} from "../../../../utils";
 
 export const FeedbackShow = () => {
 
     const {pathname} = useLocation()
-    const userId = pathname.replace(`${AdminRouteNames.ADMIN_FEEDBACK}/`, '')
-    const {data, isLoading} = userAPI.useUserShowQuery(1)
+    const feedbackId = pathname.replace(`${AdminRouteNames.ADMIN_FEEDBACK}/`, '')
+    const [feedbackMarkAsAnswered] = feedbackAPI.useFeedbackMarkAsAnsweredMutation()
+    const {data, isLoading} = feedbackAPI.useFeedbackShowQuery(feedbackId)
+
+    async function markAsAnswered() {
+        await feedbackMarkAsAnswered(feedbackId)
+            .unwrap()
+            .catch((err) => {
+                console.log(err)
+            })
+    }
 
     if (isLoading)
         return <h1>LOADING...</h1>
@@ -19,17 +30,33 @@ export const FeedbackShow = () => {
         <ShowContainer>
             <ToolbarBlock>
                 <LinkButton
-                    to={`${AdminRouteNames.ADMIN_USERS}`}
+                    to={`${AdminRouteNames.ADMIN_FEEDBACK}`}
                 >
-                    Список пользователей
+                    Список обращений
                 </LinkButton>
             </ToolbarBlock>
             <ShowBlock>
-                <TextField value={data.user_id} label={'id'}/>
-                <TextField value={data.user_email} label={'e-mail'}/>
-                <TextField value={data.user_password} label={'Пароль'}/>
-                <TextField value={data.user_name} label={'Имя'}/>
-                <TextField value={data.user_phone_number} label={'Номер телефона'}/>
+                <ShowContent>
+                    <TextField value={data.feedback_id} label={'id'}/>
+                    <DoubleFieldBlock>
+                        <FieldBlock>
+                            <TextField value={data.feedback_email} label={'e-mail'}/>
+                        </FieldBlock>
+                        <FieldBlock>
+                            <BoolField value={data.is_answer} label={'Отвечено'}/>
+                        </FieldBlock>
+                    </DoubleFieldBlock>
+                    <TextField value={data.feedback_message} label={'Обращение'}/>
+                </ShowContent>
+                <EditToolbarBlock>
+                    <Button
+                        onClick={markAsAnswered}
+                        width={150}
+                        height={50}
+                    >
+                        Пометить как отвеченное
+                    </Button>
+                </EditToolbarBlock>
             </ShowBlock>
         </ShowContainer>
     )
@@ -39,7 +66,23 @@ export const FeedbackShow = () => {
 const ShowBlock = styled.div`
   display: flex;
   flex-direction: column;
-  padding: 10px;
   border: 1px solid #9f9e9e;
   border-radius: 10px;
+`
+
+const ShowContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  padding: 40px 50px 10px;
+`
+
+const DoubleFieldBlock = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+`
+
+const FieldBlock = styled.div`
+  width: 50%;
 `
