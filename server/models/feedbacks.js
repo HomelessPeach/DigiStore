7ec848e2e1808c08b79f95df5e7c47ad6c16643b
xxlvt <sopelkaboss@gmail.com>
2022-12-14 22:15:13 +0300

    server -> models upd

--- server/models/feedbacks.js
+++ server/models/feedbacks.js
@@ -13,7 +13,7 @@ module.exports = function(sequelize, DataTypes) {
       allowNull: false,
       unique: true
     },
-    feedback_massege: {
+    feedback_message: {
       type: DataTypes.TEXT,
       allowNull: false
     },
