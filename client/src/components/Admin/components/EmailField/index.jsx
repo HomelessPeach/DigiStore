09e53c5316cae06288c8e4ddfb2e686992b13c17
xxlvt <sopelkaboss@gmail.com>
2022-12-15 02:03:42 +0300

    client -> Add new field

---
+++ client/src/components/Admin/components/EmailField/index.jsx
@@ -0,0 +1,43 @@
+import * as React from "react";
+import styled from "styled-components"
+import {ContainerBlock, LabelBlock} from "../ComponentsStyledBlocks";
+
+export const EmailField = (props) => {
+
+    const {
+        value,
+        label,
+    } = props
+
+    return (
+        <ContainerBlock>
+            {
+                (label)?
+                    <LabelBlock>{label}</LabelBlock>
+                    :null
+            }
+            <ValueBlock>
+                {(value)?
+                    <Link
+                        href={`mailto:${value}`}
+                        onClick={(event) => event.stopPropagation()}
+                    >
+                        {value}
+                    </Link>
+                    : ''
+                }
+            </ValueBlock>
+        </ContainerBlock>
+    )
+}
+
+const ValueBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`
+
+const Link = styled.a`
+  text-decoration: none;
+  color: ${({theme}) => theme.colors.tertiary};
+`
