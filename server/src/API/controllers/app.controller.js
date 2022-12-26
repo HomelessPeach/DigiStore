b35a5a7c371c6c9e409b294a28a56bd74e2a289d
xxlvt <sopelkaboss@gmail.com>
2022-12-26 22:29:43 +0300

    server -> fix

--- server/src/API/controllers/app.controller.js
+++ server/src/API/controllers/app.controller.js
@@ -4,7 +4,7 @@ class AppController {
 
     static async getReactApp(req, res, next) {
         try {
-            res.sendFile(path.join(__dirname, '../../../../public/app', 'index.html'));
+            res.sendFile(path.join(__dirname, '../../public/app', 'index.html'));
         } catch (err) {
             next(err)
         }
