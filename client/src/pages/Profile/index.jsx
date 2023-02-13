6f48f0efdc27264c580da8733f47c308a9327668
xxlvt <sopelkaboss@gmail.com>
2023-02-13 13:28:47 +0300

    client -> profile upd

--- client/src/pages/Profile/index.jsx
+++ client/src/pages/Profile/index.jsx
@@ -1,11 +1,31 @@
 import * as React from "react";
 import styled from "styled-components"
-import {NotFound} from "../NotFound";
 
 export const Profile = () => {
     return (
-        <div>
-            <NotFound/>
-        </div>
+        <ProfilePage>
+            <ProfileCardContainer>
+
+            </ProfileCardContainer>
+        </ProfilePage>
     )
 }
+
+const ProfilePage = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 60px 300px;
+`
+
+const ProfileCardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 10% 20% 5%;
+  box-shadow: 0 0 10px 0 #808080;
+  border-radius: 30px;
+  width: 100%;
+`
