db42e0dd48283ba764171d9ff95a2ee2691cf49d
xxerr <khokhlov.ta@element5ng.ru>
2023-06-19 22:01:27 +0300

    update

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -181,7 +181,7 @@ const ProductWrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
+  align-items: stretch;
   flex-wrap: wrap;
   gap: 50px;
   padding: 25px 0 100px;
